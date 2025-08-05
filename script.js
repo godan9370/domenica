f21096b0b734c9a8ba1d3f1c92186bfeae133ecf
godan9370 <dan.go@bythirdparty.com>
2025-08-06 09:56:29 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const vinyl = document.getElementById('vinyl');
 const audio = document.getElementById('audio');
+const trackSelect = document.getElementById('trackSelect');
 
-// Click vinyl image to toggle play/pause
+// Click vinyl to play/pause
 vinyl.addEventListener('click', () => {
   if (audio.paused) {
     audio.play();
   } else {
     audio.pause();
   }
+});
+
+// Change track when selection changes
+trackSelect.addEventListener('change', () => {
+  const selectedTrack = trackSelect.value;
+  audio.src = selectedTrack;
+  audio.play();
 });
